52581fb6d9e1c1f4dbc3c387880753890c710859
Natalie-Cardot <nataliecardot@gmail.com>
2018-09-25 19:36:46 -0700

    Add book and shelf component starter files

---
+++ src/components/shelf.js
@@ -0,0 +1,10 @@
+// Importing React library and saving it to a variable named 'React'
+import React from 'react'
+
+export class Shelf extends React.Component {
+  render() {
+    return (
+
+    );
+  }
+}
