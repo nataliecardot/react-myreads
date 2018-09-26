905252bb2f1c00cfd3bd29244195b1d71f99cc3f
Natalie-Cardot <nataliecardot@gmail.com>
2018-09-25 21:28:04 -0700

    Add component for floating action button, move code into components

--- src/components/book.js
+++ src/components/book.js
@@ -1,10 +1,27 @@
-// Importing React library and saving it to a variable named 'React'
-import React from 'react'
+// Importing React library and saving it to a variable named 'React'. Default (not named) import syntax
+import React, { Component } from 'react';
 
-export class Book extends React.Component {
+export default class Book extends Component {
   render() {
     return (
-
+      <li>
+        <div className="book">
+          <div className="book-top">
+            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api")' }}></div>
+            <div className="book-shelf-changer">
+              <select>
+                <option value="move" disabled>Move to...</option>
+                <option value="currentlyReading">Currently Reading</option>
+                <option value="wantToRead">Want to Read</option>
+                <option value="read">Read</option>
+                <option value="none">None</option>
+              </select>
+            </div>
+          </div>
+          <div className="book-title">1776</div>
+          <div className="book-authors">David McCullough</div>
+        </div>
+      </li>
     );
   }
 }
--- src/components/shelf.js
+++ src/components/shelf.js
@@ -1,10 +1,18 @@
 // Importing React library and saving it to a variable named 'React'
-import React from 'react'
+import React, { Component } from 'react';
+import Book from './book'; // Syntax for default export import (no curly braces)
 
-export class Shelf extends React.Component {
+export default class Shelf extends Component {
   render() {
     return (
-
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">Currently Reading</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+            <Book />
+          </ol>
+        </div>
+      </div>
     );
   }
 }
