0d8ddc9766b4d2116368f0f80ce9e10bcd039df3
Natalie-Cardot <nataliecardot@gmail.com>
2018-09-28 15:05:22 -0700

    Add comments, remove unnecessary code

--- src/components/search.js
+++ src/components/search.js
@@ -88,18 +88,17 @@ const searchTerms = [
 export default class Search extends Component {
   // Keeps search result state updated
   updateQuery(query) {
+    // Clears existing state data--the results of a previous search, if one was done. Note this is done each time a letter is entered/the search input is changed
     this.setState({results: []});
+    // If matched method returns true (there's a search search result from searchTerms that matches at least at the beginning what's entered in the search bar), executes search method from BooksAPI (imported into this component), which returns a promise that resolves to a JSON object. It takes that JSON object (the "results") and passes in the results to the checkIfSaved method. If the matched method doesn't return true, nothing is displayed because the state is already cleared, and nothing is returned because the search method is not executed
     if (this.matched(query)) {
-      // search method from BooksAPI returns a promise that resolves to a JSON object containing a collection of a maximum of 20 book objects
-      search(query.trim()).then(results => (this.checkIfSaved(results)));
-    } else {
-      this.setState({results: []});
+      search(query).then(results => (this.checkIfSaved(results)));
     }
   }
 
-  // Checks if book is already on shelf
+  // Checks if book is already on shelf with search results (from BooksAPI) that met the requirement of search input matching provided search terms. The variable notSaved stores books that don't pass the test of their id matching
   checkIfSaved = results => {
-    // filter() method creates a new array with elements that pass test implemented by provided function
+    // filter() method creates a new array with elements that pass test implemented by provided function. some() method tests whether at least one element in the array passes the test implemented by the provided callback function
     let notSaved = results.filter(nb => !this.props.books.some(ob => nb.id === ob.id));
     let alreadySaved = this.props.books.filter(ob => results.some(nb => ob.id === nb.id));
     this.setState(state => {
@@ -113,7 +112,9 @@ export default class Search extends Component {
   // Enables usage of provided search terms
   matched = query => {
     if (query) {
-      const terms = searchTerms.filter(term => term.toLowerCase().startsWith(query.toLowerCase()))
+      // Creates new array from search terms list (first converting the search terms to lower case) filtered by search terms that at least partially match (at the beginning) search input
+      const terms = searchTerms.filter(term => term.toLowerCase().startsWith(query.toLowerCase()));
+      // If there are no items in the newly created array (no matches), matched returns false. This true/false value is used in updateQuery function
       if (terms.length < 1) {
         return false
       } else {
@@ -132,16 +133,14 @@ export default class Search extends Component {
     return (
       <div className="search-books">
         <div className="search-books-bar">
-          <Link className="close-search" to='/'>
-            Close
-          </Link>
+          <Link className="close-search" to='/' />
           <div className="search-books-input-wrapper">
             <input type="text" placeholder="Search by title or author" value={query} onChange={e => this.updateQuery(e.target.value)}/>
           </div>
         </div>
         <div className="search-books-results">
           <ol className="books-grid">
-            <Book books={this.state.results} saveBook={saveBook}/>
+            <Book books={this.state.results} saveBook={saveBook} />
           </ol>
         </div>
       </div>
