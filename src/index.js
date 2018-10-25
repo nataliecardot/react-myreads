342c5e5c518b20c462bb388ca0840fb4f486c491
Natalie-Cardot <nataliecardot@gmail.com>
2018-10-24 22:38:19 -0700

    Change BrowserRouter to HashRouter

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import App from './app';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
   // Renders a Router component with history prop (a library also, like React Router, built by React Training)/object, which tracks URL changes and makes available throughout app (as long as BrowserRouter wraps the app)
-  <BrowserRouter>
+  // Update: Using HashRouter to solve blank page issue after deployment, as suggested in https://docs.google.com/document/d/12yRK6HATIZ5cJETkj0e9S3fyLnHC-JBhTzZlQerNyIE/edit
+  <HashRouter>
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById('root')
 );
