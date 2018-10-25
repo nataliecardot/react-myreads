d0f5bfdabaf57e87a03d12edd4e95e8dfa81e1eb
Natalie-Cardot <nataliecardot@gmail.com>
2018-10-24 22:40:05 -0700

    Remove import

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
 import { HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
